7c54e332e6be0ce86c1f9d0992f6503ec66a426b
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-09-28 22:30:15 -0400

    updated main with current build

--- src/com/company/cardGame/blackJack/Hand.java
+++ src/com/company/cardGame/blackJack/Hand.java
@@ -1,7 +1,6 @@
 package com.company.cardGame.blackJack;
 
 import com.company.cardGame.deck.Card;
-import com.company.cardGame.deck.Deck;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +9,9 @@ public class Hand {
     private List<Card> cards = new ArrayList<>();
     private int bet = 0;
     private Actor holder;
+
+    public static final byte PUSHPAY = 0;
+    public static final byte NORMALPAY = 1;
 
     public Hand(Actor holder) {
         this.holder = holder;
@@ -32,7 +34,7 @@ public class Hand {
         int score = 0;
         boolean haveAce11 = false;
         for (Card card : cards) {
-            int value = card.getValue();
+            int value = card.getRank();
             switch (value) {
                 case 1 -> {
                     value = score + 11 > 21 ? 1 : 11;
@@ -58,6 +60,38 @@ public class Hand {
     // pass through method
     public byte getAction() {
         return holder.getAction(this);
+    }
+
+    public int size() { return cards.size(); }
+
+    public int getBet() { return bet; }
+
+    public void placeBet() {
+        bet = holder.placeBet();
+    }
+
+    public int getBalance() { return holder.getBalance();}
+
+    public String getName() { return holder.getName(); }
+
+    // hasPair -> returns true if at least 1 pair is in the hand.
+    // isPair -> takes 2 cards and determines if pair
+    // countPair -> count all instances of pairs in a hand.
+    // checkPair -> check first two cards for pair
+    public boolean canSplit() {
+        return cards.get(0).getRank() == cards.get(1).getRank();
+    }
+
+    public void doubleBet() {
+        holder.addBalance(-bet);
+        bet *= 2;
+    }
+
+    public void payout(byte type) {
+        switch (type) {
+            case PUSHPAY -> holder.addBalance(bet);
+            case NORMALPAY -> holder.addBalance(bet * 2);
+        }
     }
 
 }
