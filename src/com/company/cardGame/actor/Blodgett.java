6cb0534128c64beab6613984d5b464aa33d6cb4f
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-10-02 20:40:20 -0400

    enable default methods from Actor

--- src/com/company/cardGame/actor/Blodgett.java
+++ src/com/company/cardGame/actor/Blodgett.java
@@ -1,6 +1,34 @@
 package com.company.cardGame.actor;
 
-public class Blodgett {
+import com.company.cardGame.blackJack.Actor;
+import com.company.cardGame.blackJack.Hand;
+
+public class Blodgett implements Actor {
 
     
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getBalance() {
+        return 0;
+    }
+
+    @Override
+    public int placeBet() {
+        return 0;
+    }
+
+    @Override
+    public byte getAction(Hand hand, Hand dealer) {
+        return 0;
+    }
+
+    @Override
+    public void addBalance(double amt) {
+
+    }
 }
